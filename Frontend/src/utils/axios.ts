122a0e22e9068ede1cf8b2194919bedc6ccb6b32
CHAWALE ADITYA SUDHAKAR <adityachawale14@gmail.com>
2025-02-02 14:43:22 +0530

    Deploy

--- Frontend/src/utils/axios.ts
+++ Frontend/src/utils/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 let token = localStorage.getItem("token")
-const API_URL = process.env.VITE_API_URL
+const API_URL = import.meta.env.VITE_API_URL
 export const axiosInstance=axios.create({
     baseURL:API_URL,
     headers:{
